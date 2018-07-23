4b5677182d0c7d050d9080efc49f18b0ae63db0d
Sam Dindyal <sam.dindyal@me.com>
2018-07-22 20:48:51 -0400

    Creates main view controller.

---
+++ SimpleCalculator/SimpleCalculatorViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SimpleCalculatorViewController.swift
+//  SimpleCalculator
+//
+//  Created by Sam Dindyal on 2018-07-22.
+//  Copyright © 2018 Sam Dindyal. All rights reserved.
+//
+
+import UIKit
+
+class SimpleCalculatorViewController: UIViewController {
+    
+}
