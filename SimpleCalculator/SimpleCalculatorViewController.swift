0d578bbba2f539322dd258104f7f449b38c301dd
Sam Dindyal <sam.dindyal@me.com>
2018-07-22 21:39:30 -0400

    Adds dictionary containing operations closures into controller.

--- SimpleCalculator/SimpleCalculatorViewController.swift
+++ SimpleCalculator/SimpleCalculatorViewController.swift
@@ -10,4 +10,32 @@ import UIKit
 
 class SimpleCalculatorViewController: UIViewController {
     
+    @IBOutlet var xLabel: UILabel!
+    @IBOutlet var yLabel: UILabel!
+    @IBOutlet var zLabel: UILabel!
+    
+    @IBAction func changeOperation(_ sender: UISegmentedControl) {
+        let operation = sender.titleForSegment(at: sender.selectedSegmentIndex)!
+        let z = operations[operation]?(1, 2)
+        print(z!)
+    }
+    
+    let operations: [String:((Int, Int) -> Int)] = [
+        "+": {
+                (x, y) -> Int in
+                    return x + y
+            },
+        "-": {
+                (x, y) -> Int in
+                    return x - y
+            },
+        "x": {
+                (x, y) -> Int in
+                    return x * y
+        },
+        "÷": {
+                (x, y) -> Int in
+                    return x / y
+            }
+    ]
 }
